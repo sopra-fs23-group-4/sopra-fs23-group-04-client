82fcd63cf7c587db5f8621dc7dc2d472ca20edc9
wigeto <remo.wiget@gmail.com>
2023-03-30 23:21:38 +0200

    s1_5: adapt StandardButton

--- src/components/ui/StandardButton.jsx
+++ src/components/ui/StandardButton.jsx
@@ -2,19 +2,17 @@ import { Button, rem } from "@mantine/core";
 
 const StandardButton = ({ children }) => (
     <Button
+        variant="gradient"
+        gradient={{ from: "teal", to: "blue", deg: 60 }}
+        size="md"
         styles={(theme) => ({
             root: {
-                backgroundColor: "#00acee",
                 border: 0,
                 height: rem(42),
                 width: rem(120),
                 "&:not([data-disabled])": theme.fn.hover({
                     backgroundColor: theme.fn.darken("#00acee", 0.05),
                 }),
-            },
-
-            leftIcon: {
-                marginRight: theme.spacing.md,
             },
         })}
     >
