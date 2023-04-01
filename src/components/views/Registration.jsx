9274df178f164d437bc392fed7e914b5e1966cd6
LexuTros <lennart@toellke.com>
2023-04-01 17:38:41 +0200

    s1_7: added the communication with the backend

--- src/components/views/Registration.jsx
+++ src/components/views/Registration.jsx
@@ -1,9 +1,34 @@
 import BaseContainer from "../ui/BaseContainer";
 import { Container, Group, PasswordInput, Stack, TextInput, Title } from "@mantine/core";
 import StandardButton from "../ui/StandardButton";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { useState } from "react";
+import { api, handleError } from "../../helpers/api";
+import User from "../../models/User";
 
 const Registration = () => {
+    const history = useHistory();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
+
+    const doRegistration = async () => {
+        try {
+            const requestBody = JSON.stringify({ username, password });
+            const response = await api.post("/users", requestBody);
+
+            // Get the returned user and update a new object.
+            const user = new User(response.data);
+
+            // Store the id into the session storage.
+            sessionStorage.setItem("token", user.token);
+
+            history.push(`/dashboard`);
+        } catch (error) {
+            alert(`Something went wrong during the Registration: \n${handleError(error)}`);
+        }
+    };
+
     return (
         <BaseContainer>
             <Container size="sm">
@@ -21,25 +46,31 @@ const Registration = () => {
                                 placeholder="username"
                                 radius="lg"
                                 size="xl"
+                                value={username}
+                                onChange={(event) => setUsername(event.currentTarget.value)}
                             />
                             <PasswordInput
                                 label="password:"
                                 placeholder="Password"
                                 radius="lg"
                                 size="xl"
+                                value={password}
+                                onChange={(event) => setPassword(event.currentTarget.value)}
                             />
                             <PasswordInput
                                 label="repeat password:"
                                 placeholder="Password"
                                 radius="lg"
                                 size="xl"
+                                value={password2}
+                                onChange={(event) => setPassword2(event.currentTarget.value)}
                             />
                         </Stack>
                     </Container>
                     <Group sx={{ paddingTop: 10 }}>
                         <StandardButton
-                            component={Link}
-                            to="/dashboard"
+                            disabled={!username || !password || !password2 || username === "" || password === "" || password2 === "" || password !== password2}
+                            onClick={() => doRegistration()}
                         >
                             Register
                         </StandardButton>
