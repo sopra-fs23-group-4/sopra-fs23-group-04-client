c0f699e9e5c833c79e096b2d3ca9a48c1de72b31
wigeto <remo.wiget@gmail.com>
2023-04-02 10:39:44 +0200

    s2_8: title Dashboard

--- src/components/views/Dashboard.jsx
+++ src/components/views/Dashboard.jsx
@@ -1,7 +1,21 @@
 import BaseContainer from "../ui/BaseContainer";
+import { Container, Stack, Title } from "@mantine/core";
 
 const Dashboard = () => {
-    return <BaseContainer></BaseContainer>;
+    return (
+        <BaseContainer>
+            <Container size="sm">
+                <Stack align="center">
+                    <Title
+                        order={1}
+                        sx={{ color: "white" }}
+                    >
+                        Welcome
+                    </Title>{" "}
+                </Stack>
+            </Container>
+        </BaseContainer>
+    );
 };
 
 export default Dashboard;
