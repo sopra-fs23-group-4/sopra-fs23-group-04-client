e6aa6c100aa8629ee8c0f68187300777ded118b6
LexuTros <lennart@toellke.com>
2023-04-03 09:37:11 +0200

    s2_10: added basic component skeleton

---
+++ src/components/views/profile/edit/Picture.jsx
@@ -0,0 +1,16 @@
+import BaseContainer from "../../../ui/BaseContainer";
+import { Container, Group, PasswordInput, Stack, TextInput, Title } from "@mantine/core";
+import StandardButton from "../../../ui/StandardButton";
+import { Link } from "react-router-dom";
+
+const Picture = () => {
+    return (
+        <BaseContainer>
+            <Container size="sm">
+                <Stack align="center"></Stack>
+            </Container>
+        </BaseContainer>
+    );
+};
+
+export default Picture;
