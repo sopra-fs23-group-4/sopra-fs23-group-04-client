3d566b0fb63a5bc3a658bc56d35bafa71538f214
LexuTros <lennart@toellke.com>
2023-04-03 09:37:24 +0200

    s2_11: added basic component skeleton

---
+++ src/components/views/profile/edit/Quote.jsx
@@ -0,0 +1,14 @@
+import BaseContainer from "../../../ui/BaseContainer";
+import { Container, Stack } from "@mantine/core";
+
+const Quote = () => {
+    return (
+        <BaseContainer>
+            <Container size="sm">
+                <Stack align="center"></Stack>
+            </Container>
+        </BaseContainer>
+    );
+};
+
+export default Quote;
