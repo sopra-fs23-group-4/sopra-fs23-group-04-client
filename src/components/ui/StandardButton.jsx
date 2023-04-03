b11f5c55b140685be290288ba9f033994a3b6ef0
LexuTros <lennart@toellke.com>
2023-04-03 12:59:31 +0200

    s1_5: made the standardButton props override standard values

--- src/components/ui/StandardButton.jsx
+++ src/components/ui/StandardButton.jsx
@@ -2,7 +2,6 @@ import { Button, rem } from "@mantine/core";
 
 const StandardButton = (props) => (
     <Button
-        {...props}
         variant="gradient"
         gradient={{ from: "#f8af05", to: "#f8af05", deg: 70 }}
         size="md"
@@ -10,9 +9,10 @@ const StandardButton = (props) => (
         styles={(theme) => ({
             root: {
                 border: 0,
-                paddingLeft: rem(20),
-                paddingRight: rem(20),
-                height: rem(42),
+                paddingTop: rem(5),
+                paddingBottom: rem(5),
+                paddingLeft: rem(15),
+                paddingRight: rem(15),
                 display: "inline-block",
                 "&:not([data-disabled])": theme.fn.hover({
                     backgroundColor: theme.fn.darken("#00acee", 0.05),
@@ -27,6 +27,7 @@ const StandardButton = (props) => (
                 opacity: 0.5,
             },
         }}
+        {...props}
     >
         {props.children}
     </Button>
