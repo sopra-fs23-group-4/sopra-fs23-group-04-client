00c44cb9b99a9c157286a6dbbdc2e068af273613
LexuTros <lennart@toellke.com>
2023-04-03 14:14:02 +0200

    setup: removed unnecessary div in GameRouter.jsx

--- src/components/routing/routers/GameRouter.jsx
+++ src/components/routing/routers/GameRouter.jsx
@@ -2,14 +2,12 @@ import { Redirect, Route } from "react-router-dom";
 
 const GameRouter = (props) => {
     return (
-        <div style={{ display: "flex", flexDirection: "column" }}>
-            <Route
-                exact
-                path={`${props.base}`}
-            >
-                <Redirect to={`${props.base}/dashboard`} />
-            </Route>
-        </div>
+        <Route
+            exact
+            path={`${props.base}`}
+        >
+            <Redirect to={`${props.base}/dashboard`} />
+        </Route>
     );
 };
 
