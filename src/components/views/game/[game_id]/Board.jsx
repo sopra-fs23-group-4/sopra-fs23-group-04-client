c81be1228ffd7d52c4afa03bafa993bfb4264036
wigeto <remo.wiget@gmail.com>
2023-04-23 18:26:50 +0200

    add icons and check if all answered #20

--- src/components/views/game/[game_id]/Board.jsx
+++ src/components/views/game/[game_id]/Board.jsx
@@ -1,32 +1,79 @@
 import { useHistory } from "react-router-dom";
-import { Button, Stack, Title } from "@mantine/core";
-import React, { useState } from "react";
+import { Checkbox as CheckIcon } from "tabler-icons-react";
+import React, { useEffect, useState } from "react";
 import BaseContainer from "../../../ui/BaseContainer";
 import StandardButton from "../../../ui/StandardButton";
+import { Edit as EditIcon } from "tabler-icons-react";
+import { Button, Stack, Title } from "@mantine/core";
+import { handleError } from "../../../../helpers/RestApi";
 
 const Board = () => {
     const history = useHistory();
 
     const [letter, setLetter] = useState("A");
-    const [categories, setCategories] = useState(["City", "Country", "FirstName", "CarBrand"]);
+    const [categories, setCategories] = useState(["City", "Country", "FirstName", "Musical Instrument"]);
+    const [answers, setAnswers] = useState(["Appenzell", "Andorra", null, "Audi"]);
 
-    const element = "Country";
-    const index = categories.indexOf(element);
-
-    console.log(`The index of "${element}" is ${index}.`);
+    useEffect(() => {
+        let isMounted = true;
+        async function fetchData() {
+            try {
+                if (isMounted) {
+                    if (!letter) {
+                        setLetter(null);
+                    }
+                    if (!categories) {
+                        setCategories(null);
+                    }
+                    if (!answers) {
+                        setAnswers(null);
+                    }
+                }
+            } catch (error) {
+                console.error(`Something went wrong while fetching the categories: \n${handleError(error)}`);
+                console.error("Details:", error);
+                alert("Something went wrong while fetching the categories! See the console for details.");
+            }
+        }
+        fetchData();
+        return () => {
+            isMounted = false;
+        };
+    }, []);
 
     const Category = ({ category }) => {
+        let index = categories.indexOf(category);
+        let iconContent = (
+            <EditIcon
+                strokeWidth={1.8}
+                color="red"
+                size={21}
+            />
+        );
+        if (answers[index]) {
+            iconContent = (
+                <CheckIcon
+                    color="green"
+                    size={20}
+                />
+            );
+        }
+
         return (
-            <div className="player container">
+            <div
+                className="player container"
+                align="center"
+            >
                 <Button
                     variant="gradient"
                     gradient={{ from: "white", to: "white", deg: 105 }}
                     radius="xl"
                     size="lg"
-                    sx={{ width: "200px", color: "Black", marginBottom: "2%" }}
+                    sx={{ minWidth: "200px", color: "Black", marginBottom: "2%" }}
                     value={category}
+                    onClick={() => history.push(`/game/board/${index}`)}
                 >
-                    {category}
+                    {category}&nbsp; {iconContent}
                 </Button>
             </div>
         );
@@ -55,7 +102,7 @@ const Board = () => {
             <StandardButton
                 position="center"
                 sx={{ marginTop: "5%" }}
-                disabled={true}
+                disabled={!answers.every((value) => value !== null)}
             >
                 DONE
             </StandardButton>
