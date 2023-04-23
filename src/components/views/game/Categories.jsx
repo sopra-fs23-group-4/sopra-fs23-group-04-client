42e54a5f18cde02f7728e093249182dafe84280f
wigeto <remo.wiget@gmail.com>
2023-04-23 11:25:53 +0200

    get all categories and save in context.jsx

--- src/components/views/game/Categories.jsx
+++ src/components/views/game/Categories.jsx
@@ -1,17 +1,41 @@
 import BaseContainer from "../../ui/BaseContainer";
 import { Chip, Group, Title } from "@mantine/core";
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import StandardButton from "../../ui/StandardButton";
 import { useHistory } from "react-router-dom";
+import { handleError, RestApi } from "../../../helpers/RestApi";
+import { Context } from "../../../context";
 
 const Categories = () => {
     const history = useHistory();
-    const categories = ["city", "country", "profession"];
-    const [categoriesSelected, setCategoriesSelected] = useState([]);
+    const context = useContext(Context);
+
+    const [categories, setCategories] = useState([]);
+    const [categoriesSelected, setCategoriesSelected] = useState(null);
+
+    useEffect(() => {
+        let isMounted = true;
+        async function fetchData() {
+            try {
+                const response = await RestApi.getAllCategories();
+                if (isMounted) {
+                    setCategories(response);
+                }
+            } catch (error) {
+                console.error(`Something went wrong while fetching the categories: \n${handleError(error)}`);
+                console.error("Details:", error);
+                alert("Something went wrong while fetching the categories! See the console for details.");
+            }
+        }
+        fetchData();
+        return () => {
+            isMounted = false;
+        };
+    }, []);
 
     const doContinue = () => {
         history.push("/game/settings");
-        sessionStorage.setItem("categories", JSON.stringify(categoriesSelected));
+        context.setCategoriesSelected(categoriesSelected);
     };
 
     const Category = ({ category }) => {
@@ -29,7 +53,7 @@ const Categories = () => {
     };
 
     return (
-        <BaseContainer>
+        <BaseContainer align="center">
             <Title color="white">Category Selection</Title>{" "}
             <Chip.Group
                 multiple
@@ -38,7 +62,10 @@ const Categories = () => {
                 defaultChecked
                 onChange={setCategoriesSelected}
             >
-                <Group sx={{ marginTop: "5%" }}>
+                <Group
+                    sx={{ marginTop: "5%" }}
+                    position="center"
+                >
                     {categories.map((category) => (
                         <Category
                             key={category}
