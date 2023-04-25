149f9efa3ef29ae980dd2be60eb3d97b6cde771b
wigeto <remo.wiget@gmail.com>
2023-04-25 22:19:00 +0200

    add round to url

--- src/components/routing/routers/InGameRouter.jsx
+++ src/components/routing/routers/InGameRouter.jsx
@@ -2,6 +2,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Lobby from "../../views/game/[game_id]/Lobby";
 import Board from "../../views/game/[game_id]/Board";
 import Answer from "../../views/game/[game_id]/Answer";
+import Letter from "../../views/game/[game_id]/Letter";
 
 const InGameRouter = () => {
     const base = "/game/:gamePin";
@@ -20,32 +21,37 @@ const InGameRouter = () => {
             />
             <Route
                 exact
-                path={`${base}/letter`}
+                path={`${base}/Lobby`}
                 component={Lobby}
             />
             <Route
                 exact
-                path={`${base}/board`}
+                path={`${base}/round/:round/letter`}
+                component={Letter}
+            />
+            <Route
+                exact
+                path={`${base}/round/:round/board`}
                 component={Board}
             />
             <Route
                 exact
-                path={`${base}/board/:answerIndex`}
+                path={`${base}/round/:round/board/:answerIndex`}
                 component={Answer}
             />
             <Route
                 exact
-                path={`${base}/voting`}
+                path={`${base}/round/:round/voting`}
                 component={Lobby}
             />
             <Route
                 exact
-                path={`${base}/votingresults`}
+                path={`${base}/round/:round/votingresults`}
                 component={Lobby}
             />
             <Route
                 exact
-                path={`${base}/score`}
+                path={`${base}/round/:round/score`}
                 component={Lobby}
             />
             <Route
