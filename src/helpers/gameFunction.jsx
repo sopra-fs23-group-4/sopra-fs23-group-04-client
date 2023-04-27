a51e194afa9b176b5667b81364b62961fcd20a8c
wigeto <remo.wiget@gmail.com>
2023-04-27 19:58:07 +0200

    replace null answers with "-"

--- src/helpers/gameFunction.jsx
+++ src/helpers/gameFunction.jsx
@@ -1,8 +1,9 @@
 export function createAnswerDictionary(categories, answers) {
     const answerDict = {};
+    const answersFinal = answers.map((value) => value ?? "-");
     for (let i = 0; i < categories.length; i++) {
         const key = categories[i];
-        answerDict[key] = answers[i];
+        answerDict[key] = answersFinal[i];
     }
     return answerDict;
 }
