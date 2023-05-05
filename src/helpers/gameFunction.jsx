fc056798fd1a66e5b38d35aaaaa5919bc83138dd
wigeto <remo.wiget@gmail.com>
2023-05-05 12:44:26 +0200

    null answers allowed

--- src/helpers/gameFunction.jsx
+++ src/helpers/gameFunction.jsx
@@ -1,9 +1,8 @@
 export function createAnswerDictionary(categories, answers) {
     const answerDict = {};
-    const answersFinal = answers.map((value) => value ?? "-");
     for (let i = 0; i < categories.length; i++) {
         const key = categories[i];
-        answerDict[key] = answersFinal[i];
+        answerDict[key] = answers[i];
     }
     return answerDict;
 }
