d815d1d23bb503094e0dfbbcbc95d585be331d0f
AlexBac <alex.bacmann9@gmail.com>
2023-05-10 18:57:14 +0200

    Changed rules page

--- src/components/views/Rules.jsx
+++ src/components/views/Rules.jsx
@@ -1,35 +1,39 @@
 import { Title, Text } from "@mantine/core";
+import StandardButton from "../ui/StandardButton";
 import BaseContainer from "../ui/BaseContainer";
+import { useHistory } from "react-router-dom";
+
 
 const Rules = () => {
+
+  const history = useHistory();
+
   return (
     <BaseContainer>
       <Title color="white" style={{ marginBottom: "1rem" }}>
-        Rules
+        Game Rules
       </Title>
       <Text color="white" size="md" style={{ marginBottom: "1rem" }}>
-        Welcome to <strong>Stadt Land PLUS</strong>, an engaging word game that
-        challenges players to list words that fit specific categories, all
-        beginning with the same letter.
-      </Text>
-      <Text color="white" size="md" style={{ marginBottom: "1rem" }}>
-        To play Stadt Land +, follow these simple steps:
+        Welcome to <strong>Stadt Land PLUS</strong>, a word game where players
+        list words fitting certain categories, all starting with the same letter.
       </Text>
       <ol style={{ color: "white", marginLeft: "1.5rem" }}>
-        <li>Register as a player, create a game, and invite friends to join your lobby using the unique gamepin.</li>
-        <li>As the host, choose from predefined categories or create your own custom categories for the game.</li>
-        <li>Set the game parameters, such as the number of rounds and the length of each round.</li>
-        <li>Once all players have joined the lobby, begin the game by clicking "Start."</li>
-        <li>A random letter of the alphabet will be generated for each round, and players will have a limited time to list words for each category starting with that letter.</li>
-        <li>After the timer runs out, players will vote on other players' answers, classifying them as correct, correct but not unique, or incorrect.</li>
+        <li>A letter from the alphabet is randomly selected each round.</li>
+        <li>Players have a set time limit to write words for each category that begin with the round's letter.</li>
+        <li>Once the time is up, players vote on other players' answers: correct, correct but not unique, or incorrect.</li>
         <li>Points are awarded based on the votes: 3 points for correct and unique answers, 1 point for correct but not unique answers, and 0 points for incorrect answers.</li>
-        <li>The player with the highest overall score after all rounds have been played is declared the winner.</li>
-        <li>For each new round, a different letter will be selected for the categories.</li>
+        <li>The player with the highest total score at the end of all rounds is the winner.</li>
+        <li>In each new round, a different letter is selected for the categories.</li>
       </ol>
       <Text color="white" size="md" style={{ marginTop: "1rem" }}>
-        Dive into the fun of <strong>Stadt Land PLUS</strong> and challenge your
-        friends to a thrilling word game experience!
+        Enjoy the thrill of <strong>Stadt Land PLUS</strong>!
       </Text>
+      <StandardButton
+        onClick={() => history.push("/dashboard")}
+        sx={{ marginTop: "3%" }}
+      >
+        Back{" "}
+      </StandardButton>
     </BaseContainer>
   );
 };
