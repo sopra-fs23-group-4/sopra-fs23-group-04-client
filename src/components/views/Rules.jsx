ba6de19c4236cb4cfa8418157a3e10c59f846b3c
wigeto <remo.wiget@gmail.com>
2023-05-13 10:03:44 +0200

    redesign Rules.jsx #61

--- src/components/views/Rules.jsx
+++ src/components/views/Rules.jsx
@@ -1,41 +1,59 @@
-import { Title, Text } from "@mantine/core";
+import { Title, Text, Box } from "@mantine/core";
 import StandardButton from "../ui/StandardButton";
 import BaseContainer from "../ui/BaseContainer";
 import { useHistory } from "react-router-dom";
 
-
 const Rules = () => {
+    const history = useHistory();
 
-  const history = useHistory();
-
-  return (
-    <BaseContainer>
-      <Title color="white" style={{ marginBottom: "1rem" }}>
-        Game Rules
-      </Title>
-      <Text color="white" size="md" style={{ marginBottom: "1rem" }}>
-        Welcome to <strong>Stadt Land PLUS</strong>, a word game where players
-        list words fitting certain categories, all starting with the same letter.
-      </Text>
-      <ol style={{ color: "white", marginLeft: "1.5rem" }}>
-        <li>A letter from the alphabet is randomly selected each round.</li>
-        <li>Players have a set time limit to write words for each category that begin with the round's letter.</li>
-        <li>Once the time is up, players vote on other players' answers: correct, correct but not unique, or incorrect.</li>
-        <li>Points are awarded based on the votes: 3 points for correct and unique answers, 1 point for correct but not unique answers, and 0 points for incorrect answers.</li>
-        <li>The player with the highest total score at the end of all rounds is the winner.</li>
-        <li>In each new round, a different letter is selected for the categories.</li>
-      </ol>
-      <Text color="white" size="md" style={{ marginTop: "1rem" }}>
-        Enjoy the thrill of <strong>Stadt Land PLUS</strong>!
-      </Text>
-      <StandardButton
-        onClick={() => history.push("/dashboard")}
-        sx={{ marginTop: "3%" }}
-      >
-        Back{" "}
-      </StandardButton>
-    </BaseContainer>
-  );
+    return (
+        <BaseContainer>
+            <Title
+                color="white"
+                style={{ marginBottom: "2%" }}
+            >
+                Game Rules
+            </Title>
+            <Box
+                align="center"
+                sx={{ background: "inherit", minWidth: "220px", border: "1px solid azure", marginTop: "0%" }}
+            >
+                <Text
+                    color="white"
+                    size="md"
+                    style={{ marginTop: "1%", marginLeft: "2%", marginRight: "2%" }}
+                >
+                    Welcome to <strong>Stadt Land PLUS</strong>, a word game where players list words fitting certain categories, all starting with the same
+                    letter.
+                </Text>
+                <Box align="left">
+                    <ol style={{ color: "white", marginTop: "3%", marginLeft: "2%", marginRight: "2%" }}>
+                        <li>A letter from the alphabet is randomly selected each round.</li>
+                        <li>For each category, players have to find a word that begins with the round's letter.</li>
+                        <li>
+                            The first player with a word for all categories can end the round for all players. The round ends automatically when the timer is
+                            up.
+                        </li>
+                        <li>Points are awarded: 3 points for correct answers, 1 point for duplicate answers, and 0 points for wrong/no answers.</li>
+                        <li>The player with the highest total score wins.</li>
+                    </ol>
+                </Box>
+                <Text
+                    color="white"
+                    size="md"
+                    style={{ marginTop: "2%", marginBottom: "2%" }}
+                >
+                    Enjoy the thrill of <strong>Stadt Land PLUS</strong>!
+                </Text>
+            </Box>
+            <StandardButton
+                onClick={() => history.push("/dashboard")}
+                sx={{ marginTop: "3%", marginBottom: "5%" }}
+            >
+                Back{" "}
+            </StandardButton>
+        </BaseContainer>
+    );
 };
 
 export default Rules;
