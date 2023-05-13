6aa0de1f235977c4c104bf2f70082498bb6833c2
LexuTros <lennart@toellke.com>
2023-05-13 17:54:04 +0200

    finished the user page with dummy values, using a table for stats #16

--- src/components/views/users/User.jsx
+++ src/components/views/users/User.jsx
@@ -1,12 +1,41 @@
 import BaseContainer from "../../ui/BaseContainer";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { handleError, RestApi } from "../../../helpers/RestApi";
-import { Title } from "@mantine/core";
+import { Paper, Table, Text, Title } from "@mantine/core";
+import StandardButton from "../../ui/StandardButton";
+import { useHistory } from "react-router-dom";
+import { StorageManager } from "../../../helpers/storageManager";
+
+export const StatEntry = (props) => {
+    return (
+        <tr>
+            <td>{props.statName}</td>
+            <td>
+                <Text
+                    inline
+                    align="right"
+                >
+                    {props.statValue}
+                </Text>{" "}
+            </td>
+        </tr>
+    );
+};
 
 const User = (props) => {
     const username = props.match.params["username"];
+    const history = useHistory();
 
     const [user, setUser] = useState({ username: "" });
+    const [stats, setStats] = useState({
+        rank: 420,
+        totalPlayedGames: 69,
+        totalWins: 420,
+        totalAnswersAnwered: 69,
+        totalCorrectAndUniqueAnswers: 420,
+        totalPointsOverall: 69,
+        mostPlayedCategory: "Cannabis Strains",
+    });
 
     useEffect(() => {
         async function fetchData() {
@@ -15,17 +44,100 @@ const User = (props) => {
                     // real code
                     let userResponse = await RestApi.getUserByUsername(username);
                     setUser(userResponse);
+
+                    let statResponse = await RestApi.getAdvancedStatistics(userResponse.id);
+                    console.log(statResponse);
+                    setStats(statResponse);
                 }
             } catch (error) {
                 console.error(`Something went wrong while fetching the user: \n${handleError(error)}`);
             }
         }
         fetchData();
-    }, [user, username]);
+    }, [user, stats, username]);
 
     return (
         <BaseContainer>
-            <Title>{user.username}</Title>
+            <Title
+                align="center"
+                order={1}
+                sx={{ color: "white", marginTop: "2%" }}
+            >
+                {user.username}
+            </Title>
+
+            <Text
+                align="center"
+                size="md"
+                color="white"
+                fw={500}
+                sx={{ width: "80%", marginBottom: "4%" }}
+            >
+                "{user.quote}"
+            </Text>
+            <Paper
+                radius="md"
+                shadow="xl"
+                p="lg"
+                bg="rgba(0, 255, 0, .1)"
+                sx={{ background: "inherit", minWidth: "220px", border: "4px solid white" }}
+            >
+                <Table
+                    sx={{ color: "white" }}
+                    fontSize="lg"
+                >
+                    <thead>
+                        <tr>
+                            <th>
+                                <Title
+                                    order={3}
+                                    inline
+                                    sx={{ color: "white" }}
+                                >
+                                    Stats:
+                                </Title>
+                            </th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <StatEntry
+                            statName={"Rank"}
+                            statValue={stats.rank}
+                        />
+                        <StatEntry
+                            statName={"Games Played"}
+                            statValue={stats.totalPlayedGames}
+                        />
+                        <StatEntry
+                            statName={"Games Won"}
+                            statValue={stats.totalWins}
+                        />
+                        <StatEntry
+                            statName={"Answers Given"}
+                            statValue={stats.totalAnswersAnwered}
+                        />
+                        <StatEntry
+                            statName={"Unique Answers"}
+                            statValue={stats.totalCorrectAndUniqueAnswers}
+                        />
+                        <StatEntry
+                            statName={"Points Overall"}
+                            statValue={stats.totalPointsOverall}
+                        />
+                        <StatEntry
+                            statName={"Most Played Category"}
+                            statValue={stats.mostPlayedCategory}
+                        />
+                    </tbody>
+                </Table>
+            </Paper>
+            <StandardButton
+                onClick={() => (StorageManager.getGamePin() ? history.push(`/game/${StorageManager.getGamePin()}/lobby`) : history.push("/dashboard"))}
+                sx={{ marginTop: "3%", marginBottom: "5%" }}
+            >
+                Back
+            </StandardButton>
         </BaseContainer>
     );
 };
