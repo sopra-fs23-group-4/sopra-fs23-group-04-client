755db86ffc0c602f856df9a4e5f90bae6f8ac5eb
wigeto <remo.wiget@gmail.com>
2023-05-17 20:31:30 +0200

    round corners Rules.jsx #61

--- src/components/views/Rules.jsx
+++ src/components/views/Rules.jsx
@@ -1,4 +1,4 @@
-import { Title, Text, Box } from "@mantine/core";
+import { Title, Text, Box, Paper } from "@mantine/core";
 import StandardButton from "../ui/StandardButton";
 import BaseContainer from "../ui/BaseContainer";
 import { useHistory } from "react-router-dom";
@@ -14,8 +14,9 @@ const Rules = () => {
             >
                 Game Rules
             </Title>
-            <Box
+            <Paper
                 align="center"
+                radius="md"
                 sx={{ background: "inherit", minWidth: "220px", border: "3px solid azure", marginTop: "0%" }}
             >
                 <Text
@@ -31,7 +32,7 @@ const Rules = () => {
                         <li>A letter from the alphabet is randomly selected each round.</li>
                         <li>For each category, players have to find a word that begins with the round's letter.</li>
                         <li>
-                            The first player with a word for all categories can end the round for all players. The round ends automatically when the timer is
+                            The first player that has answered all categories can end the round for all players. The round ends automatically when the timer is
                             up.
                         </li>
                         <li>Points are awarded: 3 points for correct answers, 1 point for duplicate answers, and 0 points for wrong/no answers.</li>
@@ -45,7 +46,7 @@ const Rules = () => {
                 >
                     Enjoy the thrill of <strong>Stadt Land PLUS</strong>!
                 </Text>
-            </Box>
+            </Paper>
             <StandardButton
                 onClick={() => history.push("/dashboard")}
                 sx={{ marginTop: "3%", marginBottom: "5%" }}
