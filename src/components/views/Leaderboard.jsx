109d9be1810f38d74f1a254452a57838fe54409b
LexuTros <lennart@toellke.com>
2023-05-17 15:06:24 +0200

    finished Leaderboard.jsx with correct ranking and link to User Page #59 , #57

--- src/components/views/Leaderboard.jsx
+++ src/components/views/Leaderboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Paper, Table, Title } from "@mantine/core";
+import { Paper, Table, Title, Text, Center } from "@mantine/core";
 import BaseContainer from "../ui/BaseContainer";
 import { StorageManager } from "../../helpers/storageManager";
 import { RestApi } from "../../helpers/RestApi";
@@ -12,22 +12,88 @@ const Leaderboard = () => {
     const history = useHistory();
 
     useEffect(() => {
+        const fetchLeaderboard = async () => {
+            try {
+                if (leaderboard.length === 0) {
+                    const data = await RestApi.getLeaderboard();
+                    setLeaderboard(data);
+
+                    // Find the index of the logged-in user
+                    const loggedInUser = StorageManager.getUsername();
+                    const position = data.findIndex((entry) => entry.username === loggedInUser);
+                    setUserPosition(position);
+                }
+            } catch (error) {
+                console.error("Error fetching leaderboard:", error);
+            }
+        };
+
         fetchLeaderboard();
-    }, []);
+    }, [leaderboard]);
 
-    const fetchLeaderboard = async () => {
-        try {
-            const data = await RestApi.getLeaderboard();
-            setLeaderboard(data);
+    let tableBodyContent = (
+        <tbody>
+            <tr key="No Players">
+                <td></td>
+                <td>No players found.</td>
+                <td></td>
+            </tr>
+        </tbody>
+    );
 
-            // Find the index of the logged-in user
-            const loggedInUser = StorageManager.getUsername();
-            const position = data.findIndex((entry) => entry.username === loggedInUser);
-            setUserPosition(position);
-        } catch (error) {
-            console.error("Error fetching leaderboard:", error);
-        }
-    };
+    if (leaderboard.length > 0) {
+        // Assign ranks to the users
+        let rank = 1;
+        let previousScore = null;
+        const usersWithRanks = leaderboard.map((user, index) => {
+            if (user.accumulatedScore !== previousScore) {
+                rank = index + 1;
+                user.rank = rank;
+            } else {
+                user.rank = rank;
+            }
+            previousScore = user.accumulatedScore;
+            return user;
+        });
+
+        tableBodyContent = (
+            <tbody>
+                {usersWithRanks.slice(0, 10).map((user) => (
+                    <tr key={user.username}>
+                        <td>
+                            <Center>{user.rank}.</Center>
+                        </td>
+                        <td>
+                            <Text
+                                onClick={() => history.push(`/profile/${user.username}`)}
+                                sx={{ cursor: "pointer" }}
+                            >
+                                {" "}
+                                {user.username}
+                            </Text>
+                        </td>
+                        <td>
+                            <Center>{user.accumulatedScore}</Center>
+                        </td>
+                    </tr>
+                ))}
+                {userPosition !== null && userPosition >= 10 && (
+                    <>
+                        <tr key={"..."}>
+                            <td></td>
+                            <td>...</td>
+                            <td></td>
+                        </tr>
+                        <tr key={usersWithRanks[userPosition].username}>
+                            <td>{usersWithRanks[userPosition].rank}.</td>
+                            <td>{usersWithRanks[userPosition].username}</td>
+                            <td>{usersWithRanks[userPosition].accumulatedScore}</td>
+                        </tr>
+                    </>
+                )}
+            </tbody>
+        );
+    }
 
     return (
         <BaseContainer>
@@ -56,31 +122,7 @@ const Leaderboard = () => {
                             <th style={{ color: "white" }}>Score</th>
                         </tr>
                     </thead>
-                    {leaderboard.length > 0 ? (
-                        <tbody>
-                            {leaderboard.slice(0, 10).map((entry, index) => (
-                                <tr key={entry.username}>
-                                    <td>{index + 1}.</td>
-                                    <td>{entry.username}</td>
-                                    <td>{entry.accumulatedScore}</td>
-                                </tr>
-                            ))}
-                            {userPosition !== null && userPosition >= 10 && (
-                                <>
-                                    <tr key={"..."}>...</tr>
-                                    <tr key={leaderboard[userPosition].username}>
-                                        <td>{userPosition + 1}.</td>
-                                        <td>{leaderboard[userPosition].username}</td>
-                                        <td>{leaderboard[userPosition].accumulatedScore}</td>
-                                    </tr>
-                                </>
-                            )}
-                        </tbody>
-                    ) : (
-                        <tbody>
-                            <tr>No players found.</tr>
-                        </tbody>
-                    )}
+                    {tableBodyContent}
                 </Table>
             </Paper>
             <StandardButton
